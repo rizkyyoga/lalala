a104cb2e33de646538525a36ecd1b98b235aeae1
rizkyyoga <rizkyyoga0@gmail.com>
2014-12-16 13:16:52 +0700

    ok

---
+++ e-ticket/application/views/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>e - ticket</title>
+</head>
+<body>
+<div id="container">
+<ul>
+<li><a href="<?php echo site_url('home/');?>">home</a></li>
+<li><a href="<?php echo site_url('now_playing/');?>">now playing</a></li>
+<li><a href="<?php echo site_url('coming_soon/');?>">coming soon</a></li>
+<li><a href="<?php echo site_url('contact_us/');?>">contact us</a></li>
+</ul>
+</div>
+<p>lorem ipsum ..</p>
+</body>
+</html>
